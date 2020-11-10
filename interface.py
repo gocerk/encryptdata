d0ad36fc59abb9dbecce18fae7ef1a0580965973
kaan <65464708+kaanfkennedy@users.noreply.github.com>
2020-11-10 18:25:48 +0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(50, 20, 681, 81))
+        self.textEdit.setObjectName("textEdit")
+        self.button = QtWidgets.QPushButton(self.centralwidget)
+        self.button.setGeometry(QtCore.QRect(220, 140, 331, 51))
+        self.button.setObjectName("button")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setGeometry(QtCore.QRect(40, 290, 711, 241))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(30, 150, 101, 41))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button.setText(_translate("MainWindow", "Encrypte"))
+        self.pushButton.setText(_translate("MainWindow", "Decrypte"))
---
+++ main.py
@@ -0,0 +1,37 @@
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import * 
+from interface import Ui_MainWindow 
+from PyQt5.QtCore import * 
+from PyQt5.QtGui import * 
+from cryptography.fernet import Fernet
+import sys
+
+
+class main(QMainWindow) : 
+    def __init__(self) : 
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.button.clicked.connect(self.clickme)
+        self.ui.pushButton.clicked.connect(self.pushclick)
+
+    def clickme (self): 
+        self.text = self.ui.textEdit.toPlainText()
+        self.key = Fernet.generate_key()
+        f = Fernet(self.key)
+        texts = self.text.encode("utf-8")
+        self.token = f.encrypt(texts)
+        self.ui.textEdit_2.setText(str(self.token))
+    
+    def pushclick(self) : 
+        
+        f = Fernet(self.key)
+        dec = f.decrypt(self.token)
+        decode = dec.decode()
+        self.ui.textEdit_2.setText(str(decode))
+        
+if __name__ == "__main__" : 
+    app = QApplication([])
+    win = main()
+    win.show()
+    app.exec_()
